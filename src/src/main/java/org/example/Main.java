299e0f7f7ba5a90af07b3afe9f23d790728936bd
kibersashka <aleksandraveldaeva4@gmail.com>
2024-12-08 23:44:49 +0300

    Внесла изменения в class Main | Вельдяева А. Д.

---
+++ src/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+public class Main {
+    public static void main(String args[]) {
+        //Массив данных о пользователях
+        User[] listUsers = UserService.create4Users();
+        //Массив данных о сообщении
+        String[] message = MessageService.create4Message(listUsers[0], listUsers[1], listUsers[2], listUsers[3]);
+
+        //Вывод информации о пользователях
+        for (int i = 0; i < listUsers.length; i++) {
+            System.out.println(listUsers[i].getNickname());
+            System.out.println(listUsers[i].getName());
+            System.out.println(listUsers[i].getNumber());
+            System.out.println(listUsers[i].getPassword() + "\n");
+        }
+
+        System.out.println();
+
+        //Вывод сообщений
+        for (int i = 0; i < message.length; i++) {
+            System.out.println(message[i]);
+        }
+    }
+}
